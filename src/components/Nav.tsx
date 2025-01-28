8bbeff275f7b3bb345a05d2934c1c43b2345aeea
mateo velasquez mejia <mvca07@gmail.com>
2025-01-27 22:52:11 -0500

    code

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,31 @@
-const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
-  return (
-    <div>Nav</div>
-  )
-};
+// TODO: Add necessary code to display the navigation bar and link between the pages
+  import { Link, useLocation } from 'react-router-dom';
 
-export default Nav;
+  const Nav = () => {
+    const currentPage = useLocation().pathname;
+  
+    return (
+      <ul className='nav nav-tabs'>
+        <li className='nav-item'>
+          <Link
+            to='/'
+            className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+          >
+            Home
+          </Link>
+        </li>
+        <li className='nav-item'>
+          <Link
+            to='/SavedCandidates'
+            className={
+              currentPage === '/SavedCandidates' ? 'nav-link active' : 'nav-link'
+            }
+          >
+            Potential Candidates
+          </Link>
+        </li>
+      </ul>
+    );
+  };
+  
+  export default Nav;
