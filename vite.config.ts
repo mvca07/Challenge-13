fa969d8a6c1eefc12c69d8835c673aeb80cbe7b8
mateo velasquez mejia <mvca07@gmail.com>
2025-01-28 19:08:39 -0500

    Server fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './env',
   plugins: [react()],
+  server: {
+      host: '0.0.0.0',
+      port: 3000,
+      open: true
+    }
 });
+
